c591aee840e6bbcd8c607a9cf831003946b711f4
Blake Haydon <blake.a.haydon@gmail.com>
2022-07-15 20:06:02 +1000

    feat: add linting and formatting support

--- UnitedEnergy/UnitedEnergy.py
+++ UnitedEnergy/UnitedEnergy.py
@@ -23,14 +23,11 @@ class API:
             "login_password": password,
         }
 
-        self.requests_session.post(
-            LOGIN_URL,
-            data=payload
-        )
+        self.requests_session.post(LOGIN_URL, data=payload)
 
     def _fetch_data(self, time_period: str, time_ago: int) -> dict:
-        """ Returns a dictionary containing the data from a time_period with some number of time_ago. Time periods can
-        be day, week, month, season and year. """
+        """Returns a dictionary containing the data from a time_period with some number of time_ago. Time periods can
+        be day, week, month, season and year."""
 
         unix_time = int(time.time())
         data_url = f"https://energyeasy.ue.com.au/electricityView/period/{time_period}/{time_ago}?_{unix_time}"
@@ -38,7 +35,7 @@ class API:
         return json.loads(result.text)
 
     def refresh_data(self) -> None:
-        """ Force refresh to get the most current data. The current day data may not be accurate (see website). """
+        """Force refresh to get the most current data. The current day data may not be accurate (see website)."""
         latest_interval = self.day_data(0)["latestInterval"]
         unix_time = int(time.time())
 
@@ -46,39 +43,39 @@ class API:
         check_refresh_url = f"https://energyeasy.ue.com.au/electricityView/latestData?lastKnownInterval={latest_interval}&_={unix_time}"
         # I think this prompts the server to update its data
         real_refresh_url = f"https://energyeasy.ue.com.au/electricityView/isElectricityDataUpdated?lastKnownInterval={latest_interval}&_={unix_time}"
-        
+
         # Run the request to see if the data needs to be updated
         responce = self.requests_session.get(check_refresh_url)
         if responce.json()["poll"] == True:
             self.requests_session.get(real_refresh_url)
 
     def day_data(self, days_ago: int) -> dict:
-        """ Returns a dictionary containing the data from some number of days_ago. The number of days_ago >= 0. """
+        """Returns a dictionary containing the data from some number of days_ago. The number of days_ago >= 0."""
         TIME_PERIOD = "day"
         return self._fetch_data(TIME_PERIOD, days_ago)
 
     def week_data(self, weeks_ago: int) -> dict:
-        """ Returns a dictionary containing the data from some number of weeks_ago. The number of weeks_ago >= 0. """
+        """Returns a dictionary containing the data from some number of weeks_ago. The number of weeks_ago >= 0."""
         TIME_PERIOD = "week"
         return self._fetch_data(TIME_PERIOD, weeks_ago)
 
     def month_data(self, months_ago: int) -> dict:
-        """ Returns a dictionary containing the data from some number of months_ago. The number of months_ago >= 0. """
+        """Returns a dictionary containing the data from some number of months_ago. The number of months_ago >= 0."""
         TIME_PERIOD = "month"
         return self._fetch_data(TIME_PERIOD, months_ago)
 
     def year_data(self, years_ago: int) -> dict:
-        """ Returns a dictionary containing the data from some number of years_ago. The number of years_ago >= 0. """
+        """Returns a dictionary containing the data from some number of years_ago. The number of years_ago >= 0."""
         TIME_PERIOD = "year"
         return self._fetch_data(TIME_PERIOD, years_ago)
 
     def season_data(self, seasons_ago: int) -> dict:
-        """ Returns a dictionary containing the data from some number of seasons_ago. The number of seasons_ago >= 0. """
+        """Returns a dictionary containing the data from some number of seasons_ago. The number of seasons_ago >= 0."""
         TIME_PERIOD = "season"
         return self._fetch_data(TIME_PERIOD, seasons_ago)
 
     def on_date_data(self, past_date: datetime.date) -> dict:
-        """ Returns a dictionary containing the data from a past_data. The past_date must be in the past or today. """
+        """Returns a dictionary containing the data from a past_data. The past_date must be in the past or today."""
         today = datetime.date.today()
 
         # Calculate the number of days ago the date is
